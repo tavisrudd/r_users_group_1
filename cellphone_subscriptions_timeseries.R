b2685f1dd8e54e608a046e2bc01d2689c6f53321
Tavis Rudd <tavis@damnsimple.com>
2011-03-23 14:11:44 -0700

    tweaks

--- cellphone_subscriptions_timeseries.R
+++ cellphone_subscriptions_timeseries.R
@@ -1,24 +1,29 @@
-# install.packages(c("ggplot2","WDI", "Cairo"))
+## This example uses the ggplot2 charting library to plot
+## cell phone subscription data from the World Bank:
+## http://data.worldbank.org/indicator/IT.CEL.SETS.P2
+
+## install.packages(c("ggplot2","WDI", "Cairo"))
 require(WDI) 
 require(ggplot2)
 require(Cairo)
 
-demo.world.bank.celldata <- function (indicator="IT.CEL.SETS.P2",
+chart.world.bank.celldata <- function (indicator="IT.CEL.SETS.P2",
                                       start=1993,
                                       end=2008,
                                       title="Mobile cellular subscriptions (per 100 people)",
                                       ylab="Subscriptions per 100 people") {
   df <- WDI(country=c("US","CA","CN", "DE","JP"), indicator=indicator, start=start, end=end)
-  p1 <- (
+  ## df stands for data.frame
+  chart <- (
          ggplot(df, mapping=aes_string(x="year", y=indicator))
-         + aes(color=country)
+         + aes(color=country) # aes stands for aesthetic
          + geom_point(stat="identity")
          + geom_smooth(stat="identity")
          + theme_bw()
          + opts(title=title)
          + ylab(ylab)
          + xlab("Year"))
-  print(p1)
+  print(chart)
 }
 
 plot.to.png <- function (chart.callback, filename, width=1200, height=800) {
@@ -27,6 +32,6 @@ plot.to.png <- function (chart.callback, filename, width=1200, height=800) {
   dev.off(chart.dev)
 }
 
-demo.world.bank.celldata() # plots to screen
+chart.world.bank.celldata() # plots to screen
 
-plot.to.png(demo.world.bank.celldata, "cellphone_chart.png")
+plot.to.png(chart.world.bank.celldata, "cellphone_chart.png")
